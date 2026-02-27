6498c7cfe12978131ae54766a1e5988b90c4a836
pasha-goit-cloud <aquascaping.kiev@gmail.com>
2026-02-27 10:01:33 +0200

    3 commit

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print('Hello World')
-print('hello Git')
+#print('Hello World')
+#print('hello Git')
+#name = input('What is your name?')
+#name = 'wendi\'s dog'
+#print (name)
+#name = input('база даних')
+#print (name)
+dict = {"key": "value"}
+#У цьому словнику перераховано загальну кількість пунктів меню
+menu_dict = {"sandwiches": {
+        "price": 6,
+        "description": "Зі свіжою шинкою та сиром"             
+} ,
+ "wraps": 4, "soup": 1}
+#У цьому словнику перераховані елементи облікового запису користувача Instagram
+user_dict = {"name": "Wendy", "active": False, "followers": 2}
+print (menu_dict["sandwiches"]["description"], menu_dict["sandwiches"]["price"])
+print (user_dict["active"]) 
